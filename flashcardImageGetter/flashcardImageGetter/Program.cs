ed756b6943564918d985e52d98f429c4a2f83339
Daniel McPherson <daniel.mcpherson@live.co.uk>
2021-03-19 18:15:56 +0000

    Moved code to function, need to make writeNouns function

--- flashcardImageGetter/flashcardImageGetter/Program.cs
+++ flashcardImageGetter/flashcardImageGetter/Program.cs
@@ -1,22 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace flashcardImageGetter
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static List<string> getNouns(string path)
         {
             // Opens the words
-            string[] words = File.ReadAllLines("C:\\Users\\User\\Documents\\2000GermanWords.txt");
+            string[] words = File.ReadAllLines(path);
 
-            // Loops through the words and finds the ones with "the "
-            for(int i = 0; i < words.Length; i++)
+            // Loops through the words and finds the ones with "The " (for nouns) and appends them to the nouns list
+            List<string> nouns = new List<string> { };
+            for (int i = 0; i < words.Length; i++)
             {
-                if(words[i].Contains("The ")) {
-                    Console.WriteLine(words[i]);
+                if (words[i].Contains("The "))
+                {
+                    nouns.Add(words[i].Replace("The ", "").Trim());
                 }
             }
+
+            foreach (string noun in nouns)
+            {
+                Console.WriteLine(noun);
+            }
+
+            return nouns;
+        }
+
+        public static void writeNouns(List<string> nouns)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> nouns = getNouns("C:\\Users\\User\\Documents\\2000GermanWords");
+            writeNouns(nouns);
+
         }
     }
 }
